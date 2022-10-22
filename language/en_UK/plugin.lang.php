048c18b34183b197437b016175540c693d1d9073
TOnin <7tonin@laposte.net>
2022-10-22 10:15:53 +0200

    l10n : populating en_UK plugin.lang.php

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -1,4 +1,53 @@
 <?php
 
+$lang['StereoZoom'] = 'StereoZoom';
 
-?>
+// home
+$lang['What StereoZoom can do for me?'] = 'What StereoZoom can do for me?';
+
+
+// help.tpl
+$lang['Keyboard usage and shortcuts'] = 'Keyboard usage and shortcuts';
+
+
+// config.tpl
+$lang['How to detect stereogram picture?'] = 'How to detect stereogram picture?';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram detection'] = 'Stereogram detection';
+$lang['Define your suffix:'] = 'Define your suffix:';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'for instance: _rl, -lr, stereo, ...';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'a possible dash followed by from 2 to 6 characters';
+$lang['Stereogram view type'] = 'Stereogram view type';
+$lang['Cross view'] = 'Cross view';
+$lang['Parallel view'] = 'Parallel view';
+$lang['Stereogram zoom'] = 'Stereogram zoom';
+$lang['Mouse wheel zoom strength:'] = 'Mouse wheel zoom strength:';
+$lang['Save Settings'] = 'Save Settings';
+
+
+// picture_sz100.tpl
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Zoom 100% / Zoom Fit&#013;[Z] key';
+$lang['Zoom +&#013;[+] key'] = 'Zoom +&#013;[+] key';
+$lang['Zoom −&#013;[−] key'] = 'Zoom −&#013;[−] key';
+$lang['Reset&#013;[R] key'] = 'Reset&#013;[R] key';
+$lang['Full screen&#013;[F] key'] = 'Full screen&#013;[F] key';
+$lang['Full window&#013;[W] key'] = 'Full window&#013;[W] key';
+$lang['Help&#013;[?] key'] = 'Help&#013;[?] key';
+$lang['Switch right and left views&#013;[X] key'] = 'Switch right and left views&#013;[X] key';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse';
+
+$lang['double click : previous'] = 'double click : previous';
+$lang['double click : next'] = 'double click : next';
+$lang['drag : move&#013;double click : previous'] = 'drag : move&#013;double click : previous';
+$lang['drag : move&#013;double click : next'] = 'drag : move&#013;double click : next';
+
+$lang['Z Fit'] = 'Z Fit';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Move'] = 'Move';
+$lang['Adjust'] = 'Adjust';
+$lang['Please wait'] = 'Please wait';
+
+
+
+// $lang[''] = '';
