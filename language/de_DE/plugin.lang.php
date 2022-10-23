6d805a5e24772f5d27068febdb9078bd2b5ff368
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-23 15:42:03 +0200

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Dateiname des Stereogrammbildes: sample%s%s%s.jpg oder 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Zoom 100% / Zoom Fit&#013;[Z]-Taste';
+$lang['Zoom −&#013;[−] key'] = 'Zoom -&#013;[-]-Taste';
+$lang['Zoom +&#013;[+] key'] = 'Zoom +&#013;[+]-Taste';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'ein möglicher Bindestrich, gefolgt von 2 bis 6 Zeichen';
+$lang['double click : next'] = 'Doppelklick : weiter';
+$lang['double click : previous'] = 'Doppelklick : vorheriges';
+$lang['drag : move&#013;double click : next'] = 'ziehen : verschieben&#013;Doppelklick : weiter';
+$lang['drag : move&#013;double click : previous'] = 'ziehen : verschieben&#013;Doppelklick : vorige';
+$lang['What StereoZoom can do for me?'] = 'Was kann StereoZoom für mich tun?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z Passform';
+$lang['Switch right and left views&#013;[X] key'] = 'Umschalten zwischen rechter und linker Ansicht&#013;[X]-Taste';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'zum Beispiel: _rl, -lr, stereo, ...';
+$lang['Stereogram zoom'] = 'Stereogramm-Zoom';
+$lang['StereoZoom'] = 'StereoZoom';
+$lang['Stereogram detection'] = 'Stereogramm-Erkennung';
+$lang['Stereogram view type'] = 'Stereogramm-Ansichtstyp';
+$lang['Parallel view'] = 'Parallele Ansicht';
+$lang['Please wait'] = 'Bitte warten';
+$lang['Reset&#013;[R] key'] = 'Reset&#013;[R]-Taste';
+$lang['Save Settings'] = 'Einstellungen speichern';
+$lang['Mouse wheel zoom strength:'] = 'Stärke des Mausrad-Zooms:';
+$lang['Move'] = 'Verschieben';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Verschieben: beide Ansichten werden mit der Maus verschoben&#013;Anpassen: eine einzelne Ansicht wird mit der Maus verschoben&#013;[Strg]-Taste + Maus';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Normaler Dateiname des Bildes: sample.jpg oder 20180101T123059_dscn9876.jpg';
+$lang['Help&#013;[?] key'] = 'Hilfe&#013;[?]-Taste';
+$lang['How to detect stereogram picture?'] = 'Wie erkennt man ein Stereobild?';
+$lang['Keyboard usage and shortcuts'] = 'Tastaturnutzung und Tastenkombinationen';
+$lang['Adjust'] = 'Anpassen';
+$lang['Cross view'] = 'Cross Ansicht';
+$lang['Define your suffix:'] = 'Definieren Sie Ihr Suffix:';
+$lang['Full screen&#013;[F] key'] = 'Vollbild&#013;[F]-Taste';
+$lang['Full window&#013;[W] key'] = 'Volles Fenster&#013;[W]-Taste';
