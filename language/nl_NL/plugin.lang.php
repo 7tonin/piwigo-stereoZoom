ae2d84506e11733ea0bfe6f96d7a1be108928e15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-10-29 12:26:57 +0200

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Adjust'] = 'Aanpassen';
+$lang['Cross view'] = 'Kruisbeeld';
+$lang['Define your suffix:'] = 'Definieer je achtervoegsel:';
+$lang['Full screen&#013;[F] key'] = 'Full scherm&#013;[F] key';
+$lang['Full window&#013;[W] key'] = 'Volledig venster&#013;[W]-toets';
+$lang['Help&#013;[?] key'] = 'Help&#013;[?] toets';
+$lang['How to detect stereogram picture?'] = 'Hoe detecteer je een stereogram?';
+$lang['Keyboard usage and shortcuts'] = 'Gebruik van het toetsenbord en sneltoetsen';
+$lang['Mouse wheel zoom strength:'] = 'Zoomsterkte van het muiswiel:';
+$lang['Move'] = 'Verplaats';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Verplaatsen: beide aanzichten worden verplaatst met de muis&#013;Aanpassen: één aanzicht wordt verplaatst met de muis&#013;[Ctrl]-toets + muis';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Normale afbeelding bestandsnaam: sample.jpg of 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = 'Parallel zicht';
+$lang['Please wait'] = 'Even geduld.';
+$lang['Reset&#013;[R] key'] = 'Reset&#013;[R] toets';
+$lang['Save Settings'] = 'Instellingen opslaan';
+$lang['StereoZoom'] = 'StereoZoom';
+$lang['Stereogram detection'] = 'Stereogram detectie';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Bestandsnaam stereogram: voorbeeld%s%s%s.jpg of 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = 'Type stereogramweergave';
+$lang['Stereogram zoom'] = 'Stereogram zoom';
+$lang['Switch right and left views&#013;[X] key'] = 'Wissel rechts en links bekijken&#013;[X] toets';
+$lang['What StereoZoom can do for me?'] = 'Wat kan StereoZoom voor mij doen?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z Fit';
+$lang['Zoom +&#013;[+] key'] = 'Zoom +&#013;[+] toets';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Zoom 100% / Zoom Fit&#013;[Z] toets';
+$lang['Zoom −&#013;[−] key'] = 'Zoom -&#013;[-] toets';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'een eventueel streepje gevolgd door 2 tot 6 tekens';
+$lang['double click : next'] = 'dubbelklik : volgende';
+$lang['double click : previous'] = 'dubbelklik : vorige';
+$lang['drag : move&#013;double click : next'] = 'slepen: verplaatsen&#013;dubbelklikken: volgende';
+$lang['drag : move&#013;double click : previous'] = 'slepen: verplaatsen&#013;dubbelklikken: vorige';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'bijvoorbeeld: _rl, -lr, stereo, ...';
