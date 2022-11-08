557563ebb43101be04034bb5876c5621d225053a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-08 13:12:56 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stereogram zoom'] = 'Zoom do estereograma';
+$lang['Switch right and left views&#013;[X] key'] = 'Alternar visualizações direita e esquerda&#013;Tecla [X]';
+$lang['What StereoZoom can do for me?'] = 'O que o StereoZoom pode fazer por mim?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Ajuste Z';
+$lang['Zoom +&#013;[+] key'] = 'Zoom +&#013;Tecla [+]';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Zoom 100% / Zoom Fit&#013;Tecla [Z]';
+$lang['Zoom −&#013;[−] key'] = 'Zoom −&#013;Tecla [−]';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'um possível traço seguido de 2 a 6 caracteres';
+$lang['double click : next'] = 'clique duas vezes: próximo';
+$lang['double click : previous'] = 'duplo clique: anterior';
+$lang['drag : move&#013;double click : next'] = 'arrastar: mover&#013;duplo clique: próximo';
+$lang['drag : move&#013;double click : previous'] = 'arrastar: mover&#013;duplo clique: anterior';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'por exemplo: _rl, -lr, estéreo, ...';
+$lang['Adjust'] = 'Ajustar';
+$lang['Cross view'] = 'Vista cruzada';
+$lang['Define your suffix:'] = 'Defina seu sufixo:';
+$lang['Full screen&#013;[F] key'] = 'Tela cheia&#013;Tecla [F]';
+$lang['Full window&#013;[W] key'] = 'Janela completa&#013;Tecla [W]';
+$lang['Help&#013;[?] key'] = 'Ajuda&#013;Tecla [?]';
+$lang['How to detect stereogram picture?'] = 'Como detectar a imagem do estereograma?';
+$lang['Keyboard usage and shortcuts'] = 'Uso do teclado e atalhos';
+$lang['Mouse wheel zoom strength:'] = 'Forçar o zoom com roda do mouse:';
+$lang['Move'] = 'Mover';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Mover: ambas as visualizações são deslocadas com o mouse&#013;Ajustar: uma única visualização é deslocada com o mouse&#013;tecla [Ctrl] + mouse';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Nome de arquivo de imagem normal: sample.jpg ou 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = 'Visualização paralela';
+$lang['Please wait'] = 'Por favor, espere';
+$lang['Reset&#013;[R] key'] = 'Redefinir&#013;Tecla [R]';
+$lang['Save Settings'] = 'Salvar configurações';
+$lang['StereoZoom'] = 'StereoZoom';
+$lang['Stereogram detection'] = 'Detecção de estereograma';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Nome do arquivo de imagem do estereograma: sample%s%s%s.jpg ou 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = 'Tipo de visualização de estereograma';
