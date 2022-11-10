b879a4a0b2bb18ab8b05784940e7d6d3b399d19b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-10 13:56:29 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['a possible dash followed by from 2 to 6 characters'] = 'hugsanlegt strik og síðan fylgja 2 til 6 stafir';
+$lang['double click : next'] = 'tvöfaldur smellur: næsta';
+$lang['double click : previous'] = 'tvöfaldur smellur: fyrri';
+$lang['How to detect stereogram picture?'] = 'Hvernig á að greina þrívíddarmynd?';
+$lang['Keyboard usage and shortcuts'] = 'Lyklaborðsnotkun og flýtivísar';
+$lang['Mouse wheel zoom strength:'] = 'Aðdráttarstyrkur músarhjóls:';
+$lang['Move'] = 'Færa';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Færa: báðar sýnir eru færðar til með músinni&#013;Stilla: ein sýn er færð til með músinni&#013;[Ctrl] takkanum + músinni';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Venjulegt myndskráarnafn: sample.jpg eða 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = 'Samsíða sýn';
+$lang['Please wait'] = 'Vinsamlega hinkrið';
+$lang['Reset&#013;[R] key'] = 'Endurstilla&#013;[R] takkinn';
+$lang['Save Settings'] = 'Vista stillingar';
+$lang['StereoZoom'] = 'Þrívíddarþysjun';
+$lang['Stereogram detection'] = 'Skynjun þrívíddarmyndar';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Skráarheiti þrívíddarmyndar: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = 'Skoðunartegund þrívíddarmyndar';
+$lang['Stereogram zoom'] = 'Þysjun þrívíddarmyndar';
+$lang['Switch right and left views&#013;[X] key'] = 'Víxla hægri og vinsti sýnar&#013;[X] lykli';
+$lang['What StereoZoom can do for me?'] = 'Hvað þrívíddarþysjun getur gert fyrir mig';
+$lang['Adjust'] = 'Stilla';
+$lang['Cross view'] = 'Krosssýn';
+$lang['Define your suffix:'] = 'Skilgreindu viðskeyti þitt:';
