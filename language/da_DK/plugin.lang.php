413c585d63b3b803644e436cf06c0231b9140537
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-07 06:27:02 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['What StereoZoom can do for me?'] = 'Hvad kan StereoZoom gøre for mig?';
+$lang['Z 100%'] = 'Z 100 %';
+$lang['Z Fit'] = 'Z-fyld';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Zoom 100 % / zoom-fyld&#013;[Z]-tast';
+$lang['Move'] = 'Flyt';
+$lang['Parallel view'] = 'Parallelvisning';
+$lang['Please wait'] = 'Vent venligst';
+$lang['Save Settings'] = 'Gem indstillinger';
+$lang['StereoZoom'] = 'StereoZoom';
+$lang['Zoom −&#013;[−] key'] = 'Zoom −&#013;[−]-tast';
+$lang['Switch right and left views&#013;[X] key'] = 'Skift højre- og venstrevisninger&#013;[X]-tast';
+$lang['Zoom +&#013;[+] key'] = 'Zoom +&#013;[+]-tast';
+$lang['Adjust'] = 'Justér';
+$lang['Cross view'] = 'Krydsvisning';
+$lang['Full screen&#013;[F] key'] = 'Fuldskærmn&#013;[F]-tast';
+$lang['Full window&#013;[W] key'] = 'Fuldvindue&#013;[W]-tast';
+$lang['Help&#013;[?] key'] = 'Hjælp&#013;[?]-tast';
+$lang['Reset&#013;[R] key'] = 'Nulstil&#013;[R]-tast';
