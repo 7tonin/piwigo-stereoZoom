dabfc4f36443ac692dc7d35d56f26f872247f730
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-22 05:22:05 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stereogram zoom'] = 'Stereogram zoom';
+$lang['Switch right and left views&#013;[X] key'] = 'Bytt høyre og venstre visning&#013;[X]-tast';
+$lang['What StereoZoom can do for me?'] = 'Hva StereoZoom kan gjøre for meg?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z passe';
+$lang['Zoom +&#013;[+] key'] = '​Zoom +&#013;[+]-tast';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'Zoom 100% / Zoom Fit&#013;[Z] tast';
+$lang['Zoom −&#013;[−] key'] = 'Zoom −&#013;[−] tast';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'en mulig strek etterfulgt av fra 2 til 6 tegn';
+$lang['double click : next'] = 'dobbeltklikk: neste';
+$lang['double click : previous'] = 'dobbeltklikk: forrige';
+$lang['drag : move&#013;double click : next'] = 'dra : flytt&#013;dobbeltklikk : neste';
+$lang['drag : move&#013;double click : previous'] = 'dra : flytt&#013;dobbeltklikk : forrige';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'for eksempel: _rl, -lr, stereo, ...';
+$lang['Adjust'] = 'Justere';
+$lang['Cross view'] = 'Kryssvisning';
+$lang['Define your suffix:'] = 'Definer tilføyelsen din:';
+$lang['Full screen&#013;[F] key'] = 'Fullskjerm&#013;[F]-tast';
+$lang['Full window&#013;[W] key'] = 'Full vindu&#013;[W]-tast';
+$lang['Help&#013;[?] key'] = 'Hjelp&#013;[?] tast';
+$lang['How to detect stereogram picture?'] = 'Hvordan oppdage stereogrambilde?';
+$lang['Keyboard usage and shortcuts'] = 'Tastaturbruk og snarveier';
+$lang['Mouse wheel zoom strength:'] = 'Musehjulets zoomstyrke:';
+$lang['Move'] = 'Flytt';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Flytt: begge visningene forskyves med musen&#013;Juster: en enkelt visning forskyves med musen&#013;[Ctrl]-tasten + musen';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Vanlig bildefilnavn: sample.jpg eller 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = 'Parallell visning';
+$lang['Please wait'] = 'Vennligst vent';
+$lang['Reset&#013;[R] key'] = 'Tilbakestillings[R]-tast';
+$lang['Save Settings'] = 'Lagre innstillinger';
+$lang['StereoZoom'] = 'StereoZoom';
+$lang['Stereogram detection'] = 'Stereogramdeteksjon';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Stereogrambildes filnavn: sample%s%s%s.jpg eller 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = 'Stereogramvisningstype';
