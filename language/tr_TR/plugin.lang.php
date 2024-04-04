6abf890bd6768daf9dd73ed01993e2da2e568c03
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-04 07:11:14 +0200

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stereogram zoom'] = 'Stereogram yakınlaştırma';
+$lang['Switch right and left views&#013;[X] key'] = 'Sağ ve sol görünümleri değiştirme#013;[X] tuşu';
+$lang['What StereoZoom can do for me?'] = 'Hangi StereoZoom benim için ne yapabilir?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z Uyma';
+$lang['Zoom +&#013;[+] key'] = 'Yakınlaştır +#013;[+] tuşu';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = '%100 Yakınlaştır / Yakınlaştır\'a Sığdır#013;[Z] tuşu';
+$lang['Zoom −&#013;[−] key'] = 'Yakınlaştır −&#013;[−] tuşu';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'ardından 2 ila 6 karakter gelen olası bir kısa çizgi';
+$lang['double click : next'] = 'çift tıkla: sonraki';
+$lang['double click : previous'] = 'çift tıkla: önceki';
+$lang['drag : move&#013;double click : next'] = 'sürükle: taşı#013;çift tıkla: ileri';
+$lang['drag : move&#013;double click : previous'] = 'sürükle: taşı#013;çift tıkla: geri';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'örneğin: _rl, -lr, stereo, ...';
+$lang['Adjust'] = 'Ayarla';
+$lang['Cross view'] = 'Çapraz görünüm';
+$lang['Define your suffix:'] = 'Sonekini belirle:';
+$lang['Full screen&#013;[F] key'] = 'Tam ekran #013;[F] tuşu';
+$lang['Full window&#013;[W] key'] = 'Tam pencere#013;[W] tuşu';
+$lang['Help&#013;[?] key'] = 'Yardım&#013;[?] tuşu';
+$lang['How to detect stereogram picture?'] = 'Stereogram resmi nasıl tespit edilir?';
+$lang['Keyboard usage and shortcuts'] = 'Klavye kullanımı ve kısayolları';
+$lang['Mouse wheel zoom strength:'] = 'Fare tekerleğinin yakınlaştırma gücü:';
+$lang['Move'] = 'Taşı';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'Taşı: her iki görünüm de fareyle değiştirilir#013;Ayarla: tek bir görünüm fareyle#013 değiştirilir;[Ctrl] tuşu + fare';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'Normal resim dosya adı: sample.jpg veya 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = 'Paralel görünüm';
+$lang['Please wait'] = 'Bekle';
+$lang['Reset&#013;[R] key'] = 'Sıfırlama#013;[R] tuşu';
+$lang['Save Settings'] = 'Ayarları Kaydet';
+$lang['StereoZoom'] = 'StereoYakınlaştırma';
+$lang['Stereogram detection'] = 'Stereogram algılama';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'Stereogram resim dosya adı: sample%s%s%s.jpg veya 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = 'Stereogram görünüm türü';
