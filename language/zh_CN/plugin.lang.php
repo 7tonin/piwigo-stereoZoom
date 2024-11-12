65792d6ad687c678c12dce1d89824fece572b03b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-12 10:16:31 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stereogram zoom'] = '立体图缩放';
+$lang['Switch right and left views&#013;[X] key'] = '切换左右视图&#013;[X]键';
+$lang['What StereoZoom can do for me?'] = 'StereoZoom 可以为我做什么？';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z 适合';
+$lang['Zoom +&#013;[+] key'] = '缩放+&#013;[+]键';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = '缩放100%/缩放适合&#013;[Z]键';
+$lang['Zoom −&#013;[−] key'] = '缩放−&#013;[−]键';
+$lang['a possible dash followed by from 2 to 6 characters'] = '可能的破折号后跟 2 到 6 个字符';
+$lang['double click : next'] = '双击：下一个';
+$lang['double click : previous'] = '双击：上一个';
+$lang['drag : move&#013;double click : next'] = '拖动：移动&#013;双击：下一个';
+$lang['drag : move&#013;double click : previous'] = '拖动：移动&#013;双击：下一个';
+$lang['for instance: _rl, -lr, stereo, ...'] = '例如：_rl, -lr, stereo, …';
+$lang['Adjust'] = '调整';
+$lang['Cross view'] = '交叉视图';
+$lang['Define your suffix:'] = '定义您的后缀：';
+$lang['Full screen&#013;[F] key'] = '全屏&#013;[F]键';
+$lang['Full window&#013;[W] key'] = '全窗口&#013;[W]键';
+$lang['Help&#013;[?] key'] = '帮助&#013;[?]键';
+$lang['How to detect stereogram picture?'] = '如何检测立体图像？';
+$lang['Keyboard usage and shortcuts'] = '键盘使用和快捷键';
+$lang['Mouse wheel zoom strength:'] = '鼠标滚轮缩放强度：';
+$lang['Move'] = '移动';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = '移动：两个视图随鼠标移动&#013;调整：单个视图随鼠标移动&#013;[Ctrl]键+鼠标';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = '普通图片文件名:sample.jpg或20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = '平行视图';
+$lang['Please wait'] = '请稍候';
+$lang['Reset&#013;[R] key'] = '重置&#013;[R]键';
+$lang['Save Settings'] = '保存设置';
+$lang['StereoZoom'] = '立体缩放';
+$lang['Stereogram detection'] = '立体图检测';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = '立体图图片文件名: sample%s%s%s.jpg或20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = '立体图视图类型';
