f91e7236b6e58ad07db5c42b3a25e0a56fdf6319
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 22:49:43 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Adjust'] = 'להתאים';
+$lang['Cross view'] = 'תצוגת צלב';
+$lang['Define your suffix:'] = 'הגדר את הסיומת שלך:';
+$lang['Full screen&#013;[F] key'] = 'מסך מלא&#013;מקש [F]';
+$lang['Full window&#013;[W] key'] = 'חלון מלא&#013;מקש [W]';
+$lang['Help&#013;[?] key'] = 'עזרה&#013;מקש [?]';
+$lang['How to detect stereogram picture?'] = 'איך לזהות תמונת סטריאוגרמה?';
+$lang['Keyboard usage and shortcuts'] = 'שימוש במקלדת וקיצורי דרך';
+$lang['Mouse wheel zoom strength:'] = 'כוח הזום של גלגל העכבר:';
+$lang['Move'] = 'להזיז';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = 'להזיז: שני התצפיות מוזחות עם העכבר&#013;להתאים: תצפית בודדת מוזחת עם העכבר&#013;מקש [Ctrl] + עכבר';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = 'שם קובץ תמונה רגיל: דוגמה.jpg או 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = 'תצפית פרללית';
+$lang['Please wait'] = 'אנא המתן';
+$lang['Reset&#013;[R] key'] = 'איפוס&#013;מקש [R]';
+$lang['Save Settings'] = 'שמירת הגדרות';
+$lang['StereoZoom'] = 'זום סטריאו';
+$lang['Stereogram detection'] = 'זיהוי סטריאוגרמה';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = 'שם קובץ תמונת סטריאוגרמה: דוגמה%s%s%s.jpg או 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = 'סוג תצפית סטריאוגרמה';
+$lang['Stereogram zoom'] = 'זום סטריאוגרמה';
+$lang['Switch right and left views&#013;[X] key'] = 'החלף בין התצפיות ימין ושמאל&#013;מקש [X]';
+$lang['What StereoZoom can do for me?'] = 'מה יכול StereoZoom לעשות בשבילי?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z התאמה';
+$lang['Zoom +&#013;[+] key'] = 'זום חיובי&#013;מקש [+]';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = 'זום 100% / התאמת זום&#013;מקש [Z]';
+$lang['Zoom −&#013;[−] key'] = 'זום שלילי&#013;מקש [-]';
+$lang['a possible dash followed by from 2 to 6 characters'] = 'מקף אפשרי מופיע עם 2 עד 6 תווים';
+$lang['double click : next'] = 'לחיצה כפולה: הבא';
+$lang['double click : previous'] = 'לחיצה כפולה: הקודם';
+$lang['drag : move&#013;double click : next'] = 'גרירה: הזזה&#013;לחיצה כפולה: הבא';
+$lang['drag : move&#013;double click : previous'] = 'גרירה: הזזה&#013;לחיצה כפולה: הקודם';
+$lang['for instance: _rl, -lr, stereo, ...'] = 'לדוגמה: _rl, -lr, סטריאו, ...';
