40a169ea0a451542194c93af8e299dfd352f2ea1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-15 06:31:28 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stereogram zoom'] = '스테레오그램 확대/축소';
+$lang['Switch right and left views&#013;[X] key'] = '좌우 화면 전환&#013;[X] 키';
+$lang['What StereoZoom can do for me?'] = 'StereoZoom의 기능은 무엇인가요?';
+$lang['Z 100%'] = 'Z 100%';
+$lang['Z Fit'] = 'Z 화면 맞추기';
+$lang['Zoom +&#013;[+] key'] = '확대&#013;[+] 키';
+$lang['Zoom 100% / Zoom Fit&#013;[Z] key'] = '100% 확대 / 화면 맞추기&#013;[Z] 키';
+$lang['Zoom −&#013;[−] key'] = '확대/축소 ?&#013;[?] 키';
+$lang['a possible dash followed by from 2 to 6 characters'] = '대시(-) 뒤에 2~6자 입력';
+$lang['double click : next'] = '더블 클릭 : 다음';
+$lang['double click : previous'] = '더블 클릭 : 이전';
+$lang['drag : move&#013;double click : next'] = '드래그 : 이동&#013;더블 클릭 : 다음';
+$lang['drag : move&#013;double click : previous'] = '드래그 : 이동&#013;더블 클릭 : 이전';
+$lang['for instance: _rl, -lr, stereo, ...'] = '예: _rl, -lr, stereo, ...';
+$lang['Adjust'] = '조정';
+$lang['Cross view'] = '크로스 뷰';
+$lang['Define your suffix:'] = '접미사 설정:';
+$lang['Full screen&#013;[F] key'] = '전체 화면&#013;[F] 키';
+$lang['Full window&#013;[W] key'] = '전체 창&#013;[W] 키';
+$lang['Help&#013;[?] key'] = '도움말&#013;[?] 키';
+$lang['How to detect stereogram picture?'] = '스테레오그램 그림을 식별하는 방법?';
+$lang['Keyboard usage and shortcuts'] = '키보드 사용법 및 단축키';
+$lang['Mouse wheel zoom strength:'] = '마우스 휠 확대/축소 강도:';
+$lang['Move'] = '이동';
+$lang['Move: both views are displaced with the mouse&#013;Adjust: a single view is displaced with the mouse&#013;[Ctrl] key + mouse'] = '이동: 두 뷰 모두 마우스로 이동&#013;조정: 단일 뷰만 마우스로 이동&#013;[Ctrl] 키 + 마우스';
+$lang['Normal picture filename: sample.jpg or 20180101T123059_dscn9876.jpg'] = '일반 이미지 파일명: sample.jpg 또는 20180101T123059_dscn9876.jpg';
+$lang['Parallel view'] = '평행 뷰';
+$lang['Please wait'] = '잠시 기다려 주세요';
+$lang['Reset&#013;[R] key'] = '초기화&#013;[R] 키';
+$lang['Save Settings'] = '설정 저장';
+$lang['StereoZoom'] = 'StereoZoom';
+$lang['Stereogram detection'] = '스테레오그램 감지';
+$lang['Stereogram picture filename: sample%s%s%s.jpg or 20180101T123059_dscn9876%s%s%s.jpg'] = '스테레오그램 이미지 파일명: sample%s%s%s.jpg 또는 20180101T123059_dscn9876%s%s%s.jpg';
+$lang['Stereogram view type'] = '스테레오그램 뷰 유형';
